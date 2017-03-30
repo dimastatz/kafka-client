535f9e6ace50dd3728c854974c72043faab6c504
Dima Stazenko <dima.stazenko@clicktale.com>
2017-03-30 14:52:32 +0300

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "kafka-client"
+version := "1.0"
+scalaVersion := "2.12.1"
+libraryDependencies += "org.apache.kafka" % "kafka-clients" % "0.10.2.0"
+
+
---
+++ src/main/scala/Boot.scala
@@ -0,0 +1,26 @@
+import scala.util.Try
+
+/**
+  * Created by dima on 3/30/2017.
+  */
+object Boot {
+  def main(args: Array[String]): Unit = {
+    println("starting kafka clients")
+
+    val topic = Try(args(1)).getOrElse("testTopic")
+    val servers = Try(args(0)).getOrElse("172.22.0.212:9092")
+
+
+    val password = Try(args(3)).getOrElse("")
+    val encryptionFile = Try(args(4)).getOrElse("")
+    val isSSL = Try(args(2).toBoolean).getOrElse(false)
+
+    val dataIn = "Some data to add"
+    val client = new KafkaClient(servers, isSSL, password, encryptionFile, true)
+    val offset = client.publish(topic, 100, dataIn)
+    println(s"data published:$dataIn, ${offset.offset()}")
+
+    val dataOut = client.consume(List(topic), true)
+    println(s"data consumed:$dataOut")
+  }
+}
